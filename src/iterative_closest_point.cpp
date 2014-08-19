cba871d9a98ca3d4fb714d23c8f038de2db41a44
Toru Tamaki <tamaki@hiroshima-u.ac.jp>
2014-08-19 15:17:38 +0900

    source/target registration done

--- src/iterative_closest_point.cpp
+++ src/iterative_closest_point.cpp
@@ -3,6 +3,8 @@
 #include <pcl/point_types.h>
 #include <pcl/registration/icp.h>
 #include <pcl/visualization/cloud_viewer.h>
+#include <pcl/filters/filter_indices.h>
+#include <pcl/common/transforms.h>
 
 #include <vtkRenderWindow.h>
 #include <vtkRendererCollection.h>
@@ -12,57 +14,54 @@ int main (int argc, char** argv)
 {
   
   pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_source (new pcl::PointCloud<pcl::PointXYZ>);
-  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_target (new pcl::PointCloud<pcl::PointXYZ>);
-
-
   if ( pcl::io::loadPLYFile<pcl::PointXYZ>("../bunny/data/bun000.ply", *cloud_source) == -1 )
   {
     PCL_ERROR ("loadPLYFile faild.");
     return (-1);
   }
-  boost::shared_ptr< pcl::visualization::PCLVisualizer > viewer (new pcl::visualization::PCLVisualizer ("3D Viewer"));
+
   
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_target (new pcl::PointCloud<pcl::PointXYZ>);
+  if ( pcl::io::loadPLYFile<pcl::PointXYZ>("../bunny/data/bun045.ply", *cloud_target) == -1 )
+  {
+    PCL_ERROR ("loadPLYFile faild.");
+    return (-1);
+  }
+
+  std::vector<int> index;
+  pcl::removeNaNFromPointCloud( *cloud_source, *cloud_source, index );
+  pcl::removeNaNFromPointCloud( *cloud_target, *cloud_target, index );
+
+
+  boost::shared_ptr< pcl::visualization::PCLVisualizer > viewer (new pcl::visualization::PCLVisualizer ("3D Viewer"));
   viewer->setBackgroundColor (0, 0, 0);
+
   pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> source_color(cloud_source, 0, 255, 0);
   viewer->addPointCloud<pcl::PointXYZ> (cloud_source, source_color, "source");
   viewer->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 1, "source");
-  viewer->initCameraParameters ();
 
+  pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> target_color(cloud_target, 255, 0, 0);
+  viewer->addPointCloud<pcl::PointXYZ> (cloud_target, target_color, "target");
+  viewer->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 1, "target");
+
+  viewer->initCameraParameters ();
   // orthographic (parallel) projection; same with pressing key 'o'
   viewer->getRenderWindow ()->GetRenderers()->GetFirstRenderer()->GetActiveCamera()->SetParallelProjection(1);
   // reset camera; same with pressing key 'r'
   viewer->getRenderWindow ()->GetRenderers()->GetFirstRenderer()->ResetCamera ();
 
 
-  while (!viewer->wasStopped ())
-  {
-    viewer->spinOnce (100);
-    boost::this_thread::sleep (boost::posix_time::microseconds (100000));
-  }
-  exit(0);
+//   while (!viewer->wasStopped ())
+//   {
+//     viewer->spinOnce (100);
+//     boost::this_thread::sleep (boost::posix_time::microseconds (100000));
+//   }
+
+  
+
   
   
   
-  
-  
-  for (size_t i = 0; i < cloud_source->points.size (); ++i)
-    std::cout << "    " <<
-    cloud_source->points[i].x << " " << cloud_source->points[i].y << " " <<
-    cloud_source->points[i].z << std::endl;
-
-  *cloud_target = *cloud_source;
-  std::cout << "size:" << cloud_target->points.size() << std::endl;
-  for (size_t i = 0; i < cloud_source->points.size (); ++i)
-    cloud_target->points[i].x = cloud_source->points[i].x + 0.7f;
-
-  std::cout << "Transformed " << cloud_source->points.size () << " data points:" << std::endl;
-
-  for (size_t i = 0; i < cloud_target->points.size (); ++i)
-    std::cout << "    " << cloud_target->points[i].x << " " <<
-    cloud_target->points[i].y << " " << cloud_target->points[i].z << std::endl;
-
-
-
   pcl::IterativeClosestPoint<pcl::PointXYZ, pcl::PointXYZ> icp;
   icp.setInputSource(cloud_source);
   icp.setInputTarget(cloud_target);
@@ -74,10 +73,22 @@ int main (int argc, char** argv)
   
   std::cout << "has converged:" << icp.hasConverged()
 	    << " score: " << icp.getFitnessScore() << std::endl;
+	    
+  Eigen::Matrix4f transformation = icp.getFinalTransformation();
+  std::cout << transformation << std::endl;
   
-  std::cout << icp.getFinalTransformation() << std::endl;
   
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_source_trans (new pcl::PointCloud<pcl::PointXYZ>);
+  pcl::transformPointCloud( *cloud_source, *cloud_source_trans, transformation );
+
   
+
+  while (!viewer->wasStopped ())
+  {
+    viewer->spinOnce (100);
+    boost::this_thread::sleep (boost::posix_time::microseconds (100000));
+  }
+
   
   return (0);
 }
